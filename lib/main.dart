a354091e0652fb06e594ab2550752e0366a43ab4
Azis202017 <azisa6980@gmail.com>
2022-12-17 01:41:30 +0700

    Mentoring

---
+++ lib/main.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: WidgetMenu(),
+    );
+  }
+}
+
+class WidgetMenu extends StatefulWidget {
+  const WidgetMenu({super.key});
+
+  @override
+  State<WidgetMenu> createState() => _WidgetMenuState();
+}
+
+class _WidgetMenuState extends State<WidgetMenu> {
+   int _selectedIndex = 0;
+  final List<Widget> _page = [
+    const Center(child: Text("Index 0 : home")),
+    const ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body : _page.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+            ),
+            label: "Profile",
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+
+          
+        },
+      ),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffFAFAFA),
+      appBar: AppBar(
+        leading: const Icon(Icons.arrow_back),
+        title: const Text('Profil Saya'),
+        backgroundColor: const Color(0xffA79EFF),
+      ),
+      body: Container(
+        margin: const EdgeInsets.only(left: 16, right: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              color: const Color(0xffFFFFFF),
+              width: 358,
+              height: 66,
+              child: Row(
+                children: [
+                  const Text('Widget 1'),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text('Abdul Azis Al Ayubbi'),
+                      Row(
+                        children: const [
+                          Icon(Icons.location_on),
+                          Text(
+                            'JL DURI SELATAN IB',
+                          ),
+                        ],
+                      ),
+                    ],
+                  ), //WIDGET KE 2
+                  const Icon(Icons.logout),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 46,
+            ),
+            const Text('Menu'),
+            const SizedBox(height: 14),
+            Container(
+                width: 358,
+                height: 45,
+                color: const Color(0xffFFFFFF),
+                child: Row(
+                  children: const [
+                    SizedBox(width: 12),
+                    Icon(Icons.email),
+                    SizedBox(width: 12),
+                    Text("Undang Teman"),
+                  ],
+                )),
+            const SizedBox(
+              height: 21,
+            ),
+            Container(
+                width: 358,
+                height: 45,
+                color: const Color(0xffFFFFFF),
+                child: Row(
+                  children: const [
+                    SizedBox(width: 12),
+                    Icon(Icons.location_on),
+                    SizedBox(width: 12),
+                    Text("Ubah alamat"),
+                  ],
+                )),
+            const SizedBox(
+              height: 21,
+            ),
+            Container(
+                width: 358,
+                height: 45,
+                color: const Color(0xffFFFFFF),
+                child: Row(
+                  children: const [
+                    SizedBox(width: 12),
+                    Icon(Icons.settings),
+                    SizedBox(width: 12),
+                    Text("Pengaturan Akun"),
+                  ],
+                )),
+            const SizedBox(
+              height: 115,
+            ),
+            SizedBox(
+              width: 358,
+              height: 45,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xffA79EFF),
+                ),
+                onPressed: () {
+                  print("Helo world");
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(Icons.logout),
+                    Text(
+                      "Keluar",
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
